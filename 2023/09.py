71451ed96c3d454dd1ede820b781e28fb202ea8d
Almaz <hello@akun.dev>
2023-12-12 11:44:38 +0900

    2023/09/02

--- 2023/09.py
+++ 2023/09.py
@@ -1,30 +1,30 @@
-def get_next(seq):
-    return [seq[i + 1] - seq[i] for i in range(len(seq) - 1)]
 
+def predict_next(seq):
+    if all([s==0 for s in seq]):
+        return 0
+    return seq[-1] + 1 * predict_next([seq[i + 1] - seq[i] for i in range(len(seq) - 1)])
 
-def predict_next(seq, last):
-    s = get_next(seq)
-    last.append(s[-1])
-    if all([s[i] == 0 for i in range(len(s))]):
-        return s, last
-    else:
-        return predict_next(s, last)
-
+def predict_prev(seq):
+    if all([s==0 for s in seq]):
+        return 0
+    return seq[0] - 1 * predict_prev([seq[i + 1] - seq[i] for i in range(len(seq) - 1)])
 
 def one(inpt):
     str_list = [line for line in inpt.split("\n") if line]
     tp_list = [[int(i) for i in line.split(" ")] for line in str_list]
     s = 0
     for tp in tp_list:
-        _, last = predict_next(tp, [])
-        s += tp[-1] + sum(last)
+        s += predict_next(tp)
     return s
 
 
 def two(inpt):
     str_list = [line for line in inpt.split("\n") if line]
-    tp_list = [[int(i) for i in line.split(" ")] for line in str_list]
-    return tp_list
+    tp_list = [[int(i) for i in line.split()] for line in str_list]
+    s = 0
+    for tp in tp_list:
+        s += predict_prev(tp)
+    return s
 
 
 if __name__ == "__main__":
